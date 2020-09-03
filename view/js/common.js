7b1ea70edf44a646a8c841f9f22fe57dccbd3851
lidope <mxsxin@yeah.net>
2020-09-03 16:55:59 +0800

    优化ajaxPost本次调取接口方法

--- view/js/common.js
+++ view/js/common.js
@@ -35,13 +35,13 @@ var http = {
     },
 
     init() {
-        let _host = ['192.168', 'file://', 'localhost', '127.0.0.1']
+        let _hostList = ['192.168', 'file://', 'localhost', '127.0.0.1']
         if (baseUrl.indexOf('leaddevelop.net') > -1) {
             http.globalData.debug = false;
         } else {
             if (!http.globalData.debug) {
-                for (let i = 0; i < _host.length; i++) {
-                    if (baseUrl.indexOf(_host[i]) > -1) {
+                for (let i = 0; i < _hostList.length; i++) {
+                    if (baseUrl.indexOf(_hostList[i]) > -1) {
                         http.globalData.debug = true;
                         break;
                     }
@@ -110,7 +110,16 @@ var http = {
         let ___baseUrl = sessionStorage.getItem('___baseUrl');
         if (___baseUrl) baseUrl = ___baseUrl;
 
-        if (baseUrl.indexOf('http') == -1) {
+        let _hostList = ['192.168', 'file://', 'localhost', '127.0.0.1'];
+        let _isHostLen = 0;
+        for (let i = 0; i < _hostList.length; i++) {
+            if (baseUrl.indexOf(_hostList[i]) > -1) {
+                _isHostLen++;
+                break;
+            }
+        }
+
+        if (baseUrl.indexOf('http') == -1 || _isHostLen) {
             http.showMessage(`<b style="color: firebrick;">失败原因:\nbaseUrl不能是本地路径</b>\n<i style="font-size: 20px; letter-spacing: 1.5px; ">Tips: 点击设置baseUrl直到页面关闭前生效</i>`, () => {
                 showPrompt();
             }, () => {
